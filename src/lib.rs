dd758116bb1f17a4ebcd0502f3f4d5347817c3cb
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2020-10-26 15:50:34 -0500

    use antialiased edges for render, add wip 2nd layer support

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use image::Rgba;
 use image::RgbaImage;
 use imageproc::geometric_transformations::warp_into_with;
 use imageproc::geometric_transformations::Interpolation;
-use std::cmp::min;
+use std::cmp::{max, min};
 
 /// Creates a Smash Ultimate Minecraft Steve inspired render from the given Minecraft skin texture.
 pub fn create_render(minecraft_skin_texture: &RgbaImage) -> RgbaImage {
@@ -17,8 +17,12 @@ pub fn create_render(minecraft_skin_texture: &RgbaImage) -> RgbaImage {
         DynamicImage::ImageRgba16(buffer) => buffer,
         _ => panic!("Expected RGBA 16 bit for UVs"),
     };
+    let uvs_layer2 = match image::load_from_memory(include_bytes!("../uvs2.png")).unwrap() {
+        DynamicImage::ImageRgba16(buffer) => buffer,
+        _ => panic!("Expected RGBA 16 bit for UVs"),
+    };
 
-    sample_texture_apply_lighting(&minecraft_skin_texture, &uvs, &lighting)
+    sample_texture_apply_lighting(&minecraft_skin_texture, &uvs, &uvs_layer2, &lighting)
 }
 
 /// Creates a render with the dimensions and alpha of the reference chara file
@@ -65,13 +69,15 @@ fn copy_alpha(target: &mut RgbaImage, source: &RgbaImage) {
 fn sample_texture_apply_lighting(
     texture: &RgbaImage,
     uvs: &ImageBuffer<Rgba<u16>, Vec<u16>>,
+    uvs_layer2: &ImageBuffer<Rgba<u16>, Vec<u16>>,
     lighting: &RgbaImage,
 ) -> RgbaImage {
     let mut output = ImageBuffer::new(uvs.dimensions().0, uvs.dimensions().1);
 
     for x in 0..output.width() {
         for y in 0..output.height() {
-            *output.get_pixel_mut(x, y) = calculate_render_pixel(x, y, uvs, lighting, texture);
+            *output.get_pixel_mut(x, y) =
+                calculate_render_pixel(x, y, uvs, uvs_layer2, lighting, texture);
         }
     }
 
@@ -82,31 +88,46 @@ fn calculate_render_pixel(
     x: u32,
     y: u32,
     uvs: &ImageBuffer<Rgba<u16>, Vec<u16>>,
+    uvs_layer2: &ImageBuffer<Rgba<u16>, Vec<u16>>,
     lighting: &RgbaImage,
     texture: &RgbaImage,
 ) -> Rgba<u8> {
-    let uv = uvs.get_pixel(x, y);
-    let (u, v, _) = normalize_rgb_u16(uv);
-    let alpha = uv[3];
+    // Get texture coordinates for both uv layers.
+    let (u1, v1, _, alpha1) = normalize_rgba_u16(uvs.get_pixel(x, y));
+    let (u2, v2, _, alpha2) = normalize_rgba_u16(uvs_layer2.get_pixel(x, y));
 
     // Flip v to transform from an origin at the bottom left (OpenGL) to top left (image).
     let (tex_width, tex_height) = texture.dimensions();
-    let (texture_x, texture_y) = interpolate_nearest(u, 1f32 - v, tex_width, tex_height);
+    let (texture_x1, texture_y1) = interpolate_nearest(u1, 1f32 - v1, tex_width, tex_height);
+    let (texture_x2, texture_y2) = interpolate_nearest(u2, 1f32 - v2, tex_width, tex_height);
 
     // Perform all calculations in floating point to avoid overflow.
-    let (tex_r, tex_g, tex_b) = normalize_rgb_u8(texture.get_pixel(texture_x, texture_y));
-    let (light_r, light_g, light_b) = normalize_rgb_u8(lighting.get_pixel(x, y));
+    let (tex_r1, tex_g1, tex_b1, tex_a1) =
+        normalize_rgba_u8(texture.get_pixel(texture_x1, texture_y1));
+    let (tex_r2, tex_g2, tex_b2, tex_a2) =
+        normalize_rgba_u8(texture.get_pixel(texture_x2, texture_y2));
+
+    let (light_r1, light_g1, light_b1, _) = normalize_rgba_u8(lighting.get_pixel(x, y));
 
     // The lighting pass is scaled down by a factor of 0.25 to fit into 8 bits per channel.
     // Multiplying by 4 is a bit too bright, so use 2 instead.
     let apply_lighting = |color: f32, light: f32| color * light * 2f32;
-    let get_result = |color, light| to_u8_clamped(apply_lighting(color, light));
+    let alpha_blend = |val1: f32, val2: f32, alpha: f32| val1 * (1f32 - alpha) + val2 * alpha;
+
+    // TODO: Alpha blend second layer.
+    let get_result = |color1, color2, light, alpha| apply_lighting(color1, light);
+
+    let r_final = get_result(tex_r1, tex_r2, light_r1, tex_a2);
+    let g_final = get_result(tex_g1, tex_g2, light_g1, tex_a2);
+    let b_final = get_result(tex_b1, tex_b2, light_b1, tex_a2);
+    // TODO: Combine alpha from both layers.
+    let a_final = alpha1;
 
     Rgba([
-        get_result(tex_r, light_r),
-        get_result(tex_g, light_g),
-        get_result(tex_b, light_b),
-        alpha as u8,
+        to_u8_clamped(r_final),
+        to_u8_clamped(g_final),
+        to_u8_clamped(b_final),
+        to_u8_clamped(a_final),
     ])
 }
 
@@ -118,23 +139,25 @@ fn interpolate_nearest(x: f32, y: f32, width: u32, height: u32) -> (u32, u32) {
     (nearest(x, width), nearest(y, height))
 }
 
-fn normalize_rgb_u8(pixel: &Rgba<u8>) -> (f32, f32, f32) {
+fn normalize_rgba_u8(pixel: &Rgba<u8>) -> (f32, f32, f32, f32) {
     // 0u16 -> 0.0f32, 65535u16 -> 1.0f32
     let normalize = |u| u as f32 / 255f32;
     (
         normalize(pixel[0]),
         normalize(pixel[1]),
         normalize(pixel[2]),
+        normalize(pixel[3]),
     )
 }
 
-fn normalize_rgb_u16(pixel: &Rgba<u16>) -> (f32, f32, f32) {
+fn normalize_rgba_u16(pixel: &Rgba<u16>) -> (f32, f32, f32, f32) {
     // 0u16 -> 0.0f32, 65535u16 -> 1.0f32
     let normalize = |u| u as f32 / 65535f32;
     (
         normalize(pixel[0]),
         normalize(pixel[1]),
         normalize(pixel[2]),
+        normalize(pixel[3]),
     )
 }
 
@@ -161,7 +184,6 @@ mod tests {
 
     #[test]
     fn test_interpolate_nearest_out_of_bounds() {
-        assert_eq!(interpolate_nearest(-1f32, -1f32, 8u32, 8u32), (0u32, 0u32));
         assert_eq!(interpolate_nearest(0f32, 1.5f32, 8u32, 8u32), (0u32, 7u32));
         assert_eq!(interpolate_nearest(1.5f32, 0f32, 8u32, 8u32), (7u32, 0u32));
         assert_eq!(
@@ -173,24 +195,24 @@ mod tests {
     #[test]
     fn test_normalize_u8() {
         assert_eq!(
-            normalize_rgb_u8(&Rgba([0u8, 0u8, 0u8, 0u8])),
-            (0f32, 0f32, 0f32)
+            normalize_rgba_u8(&Rgba([0u8, 0u8, 0u8, 0u8])),
+            (0f32, 0f32, 0f32, 0f32)
         );
         assert_eq!(
-            normalize_rgb_u8(&Rgba([255u8, 255u8, 255u8, 255u8])),
-            (1f32, 1f32, 1f32)
+            normalize_rgba_u8(&Rgba([255u8, 255u8, 255u8, 255u8])),
+            (1f32, 1f32, 1f32, 1f32)
         );
     }
 
     #[test]
     fn test_normalize_u16() {
         assert_eq!(
-            normalize_rgb_u16(&Rgba([0u16, 0u16, 0u16, 0u16])),
-            (0f32, 0f32, 0f32)
+            normalize_rgba_u16(&Rgba([0u16, 0u16, 0u16, 0u16])),
+            (0f32, 0f32, 0f32, 0f32)
         );
         assert_eq!(
-            normalize_rgb_u16(&Rgba([65535u16, 65535u16, 65535u16, 65535u16])),
-            (1f32, 1f32, 1f32)
+            normalize_rgba_u16(&Rgba([65535u16, 65535u16, 65535u16, 65535u16])),
+            (1f32, 1f32, 1f32, 1f32)
         );
     }
 
